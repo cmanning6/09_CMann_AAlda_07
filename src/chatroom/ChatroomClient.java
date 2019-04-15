20ef61a62a17f9545904f2a7c99c13d47e7303da
Chad <cmanning6@csub.edu>
2019-04-15 16:22:07 -0700

    Finished. Allows selective messages

--- src/chatroom/ChatroomClient.java
+++ src/chatroom/ChatroomClient.java
@@ -7,14 +7,12 @@ package chatroom;
 /**
  *
  * @author hwang
+ * Modified by: Chad Manning
+ * Course: CMPS 3390
  */
-import java.io.IOException;
 import java.net.Socket;
-import java.net.ServerSocket;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import javax.swing.*;
 
@@ -22,17 +20,18 @@ import javax.swing.*;
 class CAgent implements Runnable{
     ObjectInputStream in;
     JTextArea  taMsgBrd;
-    JFrame client;
-    JPanel panel;
+    String userName;
+    ChatroomClient cl;
     ArrayList<String> currUsers;
-    ArrayList<JCheckBox> userBoxes;
+    JCheckBox[] userBoxes;
 
-    public CAgent ( ObjectInputStream in, JTextArea brd, JPanel list) {
+    public CAgent ( ObjectInputStream in, ChatroomClient client, JCheckBox[] cbk) {
         this.in = in;
-        taMsgBrd = brd;
-        panel = list;
+        cl = client;
+        taMsgBrd = cl.taMsgBrd;
+        userName = cl.userName;
         currUsers = new ArrayList();
-        userBoxes = new ArrayList();
+        userBoxes = cbk;
     }
     public void run ()  {
         Object obj = null;
@@ -45,15 +44,23 @@ class CAgent implements Runnable{
                   case "JOIN" :
                   case "DISCONNECT":
                     currUsers = (ArrayList<String>) in.readObject();
-                    userBoxes.add(new JCheckBox());
-                    userBoxes.get(0).setText(currUsers.get(0));
-                    panel.add(userBoxes.get(0));
+                    currUsers.remove(userName);
+                    for (int i=0; i < 10; ++i)
+                        if (i > currUsers.size()-1) {
+                            userBoxes[i].setText("");
+                            userBoxes[i].setSelected(false);
+                            userBoxes[i].setEnabled(false);
+                        }
+                        else {
+                            userBoxes[i].setText(currUsers.get(i));
+                            userBoxes[i].setSelected(true);
+                            userBoxes[i].setEnabled(true);
+                        }
                     break;
                   case "WRITE" :
                     taMsgBrd.append ( msg.split(" ", 2)[1] + "\n");
                     break;
               }
-              taMsgBrd.append(currUsers.toString() + "\n");
             }
         }
         catch (Exception e) { e.printStackTrace(); }
@@ -62,6 +69,8 @@ class CAgent implements Runnable{
 
 public class ChatroomClient extends JFrame {
     String userName = "John Doe";
+    JCheckBox[] cbk = new JCheckBox[10];
+    ArrayList<String> sendList = new ArrayList();
     /**
      * Creates new form ChatroomClient
      */
@@ -85,10 +94,21 @@ public class ChatroomClient extends JFrame {
         tfName = new javax.swing.JTextField();
         tfMessage = new javax.swing.JTextField();
         jPanel1 = new javax.swing.JPanel();
+        jCheckBox1 = new javax.swing.JCheckBox();
         jCheckBox2 = new javax.swing.JCheckBox();
+        jCheckBox3 = new javax.swing.JCheckBox();
+        jCheckBox4 = new javax.swing.JCheckBox();
+        jCheckBox5 = new javax.swing.JCheckBox();
+        jCheckBox6 = new javax.swing.JCheckBox();
+        jCheckBox7 = new javax.swing.JCheckBox();
+        jCheckBox8 = new javax.swing.JCheckBox();
+        jCheckBox9 = new javax.swing.JCheckBox();
+        jCheckBox10 = new javax.swing.JCheckBox();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Chatroom Client");
+        setResizable(false);
 
         taMsgBrd.setEditable(false);
         taMsgBrd.setColumns(20);
@@ -110,64 +130,116 @@ public class ChatroomClient extends JFrame {
             }
         });
 
-        jPanel1.addContainerListener(new java.awt.event.ContainerAdapter() {
-            public void componentAdded(java.awt.event.ContainerEvent evt) {
-                jPanel1ComponentAdded(evt);
-            }
-        });
+        jCheckBox1.setEnabled(false);
 
-        jCheckBox2.setText("jCheckBox2");
+        jCheckBox2.setEnabled(false);
+
+        jCheckBox3.setEnabled(false);
+
+        jCheckBox4.setEnabled(false);
+
+        jCheckBox5.setEnabled(false);
+
+        jCheckBox6.setEnabled(false);
+
+        jCheckBox7.setEnabled(false);
+
+        jCheckBox8.setEnabled(false);
+
+        jCheckBox9.setEnabled(false);
+
+        jCheckBox10.setEnabled(false);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 130, Short.MAX_VALUE)
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addContainerGap()
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jCheckBox1)
                     .addComponent(jCheckBox2)
-                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addComponent(jCheckBox3)
+                    .addComponent(jCheckBox4)
+                    .addComponent(jCheckBox5)
+                    .addComponent(jCheckBox6)
+                    .addComponent(jCheckBox7)
+                    .addComponent(jCheckBox8)
+                    .addComponent(jCheckBox9)
+                    .addComponent(jCheckBox10))
+                .addGap(0, 53, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
-            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(jPanel1Layout.createSequentialGroup()
-                    .addGap(135, 135, 135)
-                    .addComponent(jCheckBox2)
-                    .addContainerGap(165, Short.MAX_VALUE)))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jCheckBox1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jCheckBox2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jCheckBox3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jCheckBox4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jCheckBox5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jCheckBox6)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jCheckBox7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jCheckBox8)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jCheckBox9)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jCheckBox10)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
+
+        jCheckBox1.getAccessibleContext().setAccessibleName("");
+        jCheckBox2.getAccessibleContext().setAccessibleName("");
+        jCheckBox3.getAccessibleContext().setAccessibleName("");
+        jCheckBox4.getAccessibleContext().setAccessibleName("");
+        jCheckBox5.getAccessibleContext().setAccessibleName("");
+        jCheckBox6.getAccessibleContext().setAccessibleName("");
+
+        jLabel1.setText("Users");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(tfName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(tfMessage))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 445, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 324, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(tfName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(tfMessage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(15, Short.MAX_VALUE))
+                .addGap(24, 24, 24)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 324, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 303, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(tfName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(tfMessage, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
+
+        jPanel1.getAccessibleContext().setAccessibleName("");
+        jPanel1.getAccessibleContext().setAccessibleDescription("");
+        jPanel1.getAccessibleContext().setAccessibleParent(jPanel1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -177,12 +249,23 @@ public class ChatroomClient extends JFrame {
         tfName.setEnabled (false);
         tfMessage.setEnabled(true);
         userName = tfName.getText();
+        cbk[0] = jCheckBox1;
+        cbk[1] = jCheckBox2;
+        cbk[2] = jCheckBox3;
+        cbk[3] = jCheckBox4;
+        cbk[4] = jCheckBox5;
+        cbk[5] = jCheckBox6;
+        cbk[6] = jCheckBox7;
+        cbk[7] = jCheckBox8;
+        cbk[8] = jCheckBox9;
+        cbk[9] = jCheckBox10;
+
         try {
             System.out.println(host);
             sk = new Socket( host, port); 
             in = new ObjectInputStream(sk.getInputStream());
             out = new ObjectOutputStream( sk.getOutputStream() );
-            new Thread ( new CAgent( in, taMsgBrd, jPanel1)).start();
+            new Thread ( new CAgent( in, this, cbk)).start();
             out.writeObject(userName);
         } catch ( Exception e ) { e.printStackTrace();}
     }//GEN-LAST:event_tfNameActionPerformed
@@ -190,16 +273,19 @@ public class ChatroomClient extends JFrame {
     private void tfMessageActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tfMessageActionPerformed
         // TODO add your handling code here
         try {
-            out.writeObject( tfName.getText() + ": "+ tfMessage.getText() );
+            //If user is selected add them to send list
+            sendList.add(userName);
+            for (JCheckBox bx : cbk)
+                if (bx.isEnabled() && bx.isSelected())
+                    sendList.add(bx.getText());
+            
+            String msg =  tfName.getText() + ": "+ tfMessage.getText(); 
+            out.writeObject(sendList.clone());
+            out.writeObject(msg);
             tfMessage.setText("");
+            sendList.clear();
         } catch (Exception e ) { }
     }//GEN-LAST:event_tfMessageActionPerformed
-
-    private void jPanel1ComponentAdded(java.awt.event.ContainerEvent evt) {//GEN-FIRST:event_jPanel1ComponentAdded
-        // TODO add your handling code here:
-        revalidate();
-        repaint();
-    }//GEN-LAST:event_jPanel1ComponentAdded
 
     /**
      * @param args the command line arguments
@@ -244,10 +330,20 @@ public class ChatroomClient extends JFrame {
     String host = "localhost";
     int    port = 9999;
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JCheckBox jCheckBox10;
     private javax.swing.JCheckBox jCheckBox2;
+    private javax.swing.JCheckBox jCheckBox3;
+    private javax.swing.JCheckBox jCheckBox4;
+    private javax.swing.JCheckBox jCheckBox5;
+    private javax.swing.JCheckBox jCheckBox6;
+    private javax.swing.JCheckBox jCheckBox7;
+    private javax.swing.JCheckBox jCheckBox8;
+    private javax.swing.JCheckBox jCheckBox9;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea taMsgBrd;
+    public javax.swing.JTextArea taMsgBrd;
     private javax.swing.JTextField tfMessage;
     private javax.swing.JTextField tfName;
     // End of variables declaration//GEN-END:variables
--- src/chatroom/ChatroomServer.java
+++ src/chatroom/ChatroomServer.java
@@ -6,19 +6,18 @@
 package chatroom;
 
 import java.io.IOException;
-import java.net.Socket;
 import java.net.ServerSocket;
-import java.util.Vector;
 import java.io.ObjectOutputStream;
 import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.HashMap;
+import java.util.Map;
 /**
  *
  * @author hwang
+ * Modified by: Chad Manning
+ * Course: CMPS 3390
  */
 public class ChatroomServer {
     static Socket       sk  = null;
@@ -47,32 +46,33 @@ public class ChatroomServer {
 
 class  SharedOuts {
     
-    ArrayList<ObjectOutputStream> vOut =  null;
     ArrayList<String> users = null;
+    Map<String, ObjectOutputStream> uMap;
 
     
     public SharedOuts() {
-	vOut = new ArrayList();
         users = new ArrayList();
+        uMap = new HashMap();
     }
     public void add( ObjectOutputStream out, String uN ) { 
-        vOut.add( out );
         users.add(uN);
+        uMap.put(uN, out);
     }
-    public void remove (int index) { 
-        vOut.remove(vOut.get(index));
-        users.remove(users.get(index));
+    public void remove (String uN) { 
+        users.remove(uN);
+        uMap.remove(uN);
     }
-    public int size() { return vOut.size(); }
+    public int size() { return uMap.size(); }
 
-    public void broadcast( Object obj ) {
-	Iterator<ObjectOutputStream>  itr = vOut.iterator();
-        Iterator<String> uItr = users.iterator();
-
-	while ( itr.hasNext() ) {
+    /**
+     * Sends message to every user regardless of sendList
+     * @param obj : obj to be broadcasted to all users 
+     */
+    public void broadcast( Object obj) {
+	for ( String u : users ) {
             try {
                 System.out.println("Writing: " + obj);
-                itr.next().writeObject(obj);
+                uMap.get(u).writeObject(obj);
             } catch (IOException ex) {
 		ex.printStackTrace();
             }
@@ -80,10 +80,41 @@ class  SharedOuts {
         System.out.print("Current Users: " + users.toString() + "\n");
     }
     
+    /**
+     * Sends message to only users on the sendList
+     * @param obj : message to be sent
+     * @param sendList : Users who will receive message
+     */
+    public void selectiveBroadcast( Object obj, ArrayList sendList) {
+	for ( String usr : users ) {
+            try {
+                if (sendList.contains(usr)) {
+                    System.out.println("Writing: " + obj);
+                    uMap.get(usr).writeObject(obj);
+                }
+            } catch (IOException ex) {
+		ex.printStackTrace();
+            }
+        }
+        System.out.print("Current Users: " + users.toString() + "\n");
+    }
+    
+    /**
+     * Sends updated user list to all clients after a connection or disconnection
+     * @param evnt : either a JOIN or DISCONNECT string
+     */
     public void updateUsers(String evnt) {
         //get users into a plain string with only commas
-        broadcast(evnt);
-        broadcast(users.clone());
+	for ( String u : users ) {
+            try {
+                System.out.println("Writing: " + evnt);
+                uMap.get(u).writeObject(evnt);
+                uMap.get(u).writeObject(users.clone());
+            } catch (IOException ex) {
+		ex.printStackTrace();
+            }
+        }
+        System.out.print("Current Users: " + users.toString() + "\n");
     }
 }
 
@@ -94,6 +125,7 @@ class SAgent implements Runnable {
     ObjectInputStream   in = null;
     SharedOuts sout = null;
     String userName = "John Doe";
+    ArrayList<String> sendList = null;
 
     public SAgent ( Socket sk, SharedOuts sout ) {
 	this.sk = sk; this.sout = sout;
@@ -111,13 +143,13 @@ class SAgent implements Runnable {
         sout.broadcast("WRITE " + userName + " has connected!");
         while (true ) {
 
-                Object obj = in.readObject() ;
-                sout.broadcast( "WRITE " + obj + " ( outs = " + sout.size() + ")" );
+                sendList = (ArrayList<String>) in.readObject();
+                sout.selectiveBroadcast( "WRITE " + in.readObject(), sendList);
         }
     } 
     catch ( Exception e) {}
     finally {
-        sout.remove(sout.users.indexOf(userName));
+        sout.remove(userName);
         sout.updateUsers( "DISCONNECT");
         sout.broadcast("WRITE " + userName + " has disconnected.");
     }
